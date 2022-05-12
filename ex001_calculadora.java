c1159d32153ed1cbddbf5cd3b962e3b5553714fe
edson-araujo <edson-456@hotmail.com>
2022-05-11 22:28:20 -0300

    primeiro commit

---
+++ ex001_calculadora.java
@@ -0,0 +1,29 @@
+
+public class ex001_calculadora {
+
+	public static void soma(double numero1, double numero2) {
+		
+		double resultado = numero1 + numero2;
+		System.out.println("A soma de " + numero1 + " + " + numero2 + " é " + resultado);
+	}
+	public static void subtracao (double numero1, double numero2) {
+		double resultado = numero1 - numero2;
+		System.out.println("A subtração de " + numero1 + " - " + numero2 + " é " + resultado);
+	}
+	public static void multiplicacao (double numero1, double numero2) {
+		double resultado = numero1 * numero2;
+		System.out.println("A multiplicação de " + numero1 + " * " + numero2 + " é " + resultado);
+	}
+	public static void divisao (double numero1, double numero2) {
+		double resultado = numero1 / numero2;
+		System.out.println("A divisão de " + numero1 + " / " + numero2 + " é " + resultado);
+	}
+	
+	public static void main(String args[]) {
+		soma(0, 0);
+		subtracao(10, 5);
+		ex001_calculadora.multiplicacao(2, 10);
+		ex001_calculadora.divisao(10, 2);
+	}
+	
+}
