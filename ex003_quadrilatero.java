fdd4f91c8a2d7370756550ba0f58bf44cce11c38
edson-araujo <edson-456@hotmail.com>
2022-05-12 01:04:33 -0300

    adicionar exercicio 004

---
+++ ex003_quadrilatero.java
@@ -0,0 +1,27 @@
+
+public class ex003_quadrilatero {
+
+	public class QuadriLatero {
+
+		public static void area(double lado) {
+			System.out.println("Área do quadrado: " + lado * lado);
+		}
+		public static void area(double lado1, double lado2) {
+			System.out.println("Área do retângulo: " + lado1 * lado2);
+		}
+		public static void area(double baseMaior, double baseMenor, double altura) {
+			System.out.println("Área do trapézio: " + ((baseMaior + baseMenor) * altura) / 2);
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		//Quadrilatero
+		System.out.println("Exercicio quadrilatero");
+		QuadriLatero.area(3);
+		QuadriLatero.area(5, 5);
+		QuadriLatero.area(7, 8, 9);
+		
+	}
+
+}
